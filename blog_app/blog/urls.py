1189bb2215b5d90cdec81701825321c10cbacb69
suchir_18 <97435643+suchir-cyber@users.noreply.github.com>
2024-12-16 12:20:26 +0530

    created URL for contact

--- blog_app/blog/urls.py
+++ blog_app/blog/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
-    path('about/',views.about,name='blog-about')
+    path('about/',views.about,name='blog-about'),
+    path('help/',views.contact,name='blog-contact')
 ]
